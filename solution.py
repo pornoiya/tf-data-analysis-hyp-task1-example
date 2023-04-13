98b48a084e96ca95c5186e6a32df5214819480c3
pornoiya <49945883+pornoiya@users.noreply.github.com>
2023-04-13 22:24:56 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -13,10 +13,6 @@ def solution(x_success: int,
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
     alpha = 0.06
-    p1 = x_success / x_cnt
-    p2 = y_success / y_cnt
-    p = (x_success + y_success) / (x_cnt + y_cnt)
-    z = (p1 - p2)/(p * (1-p) * ((1/x_cnt) + (1/y_cnt))) ** 0.5
-    p_val = norm.sf(z)
-    
-    return p_val < alpha # Ваш ответ, True или False
+    mean1 = x_success/ x_cnt
+    mean2 = y_success/ y_cnt
+    return mean2-mean1 < alpha
