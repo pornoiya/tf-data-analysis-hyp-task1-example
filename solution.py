02fc6bf5cb68e040c86c78dfd05777b9092e337a
pornoiya <49945883+pornoiya@users.noreply.github.com>
2023-04-13 22:45:38 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from scipy.stats import norm
+from statsmodels.stats.proportion import proportions_ztest
 
 
 chat_id = 146155552 # Ваш chat ID, не меняйте название переменной
@@ -12,7 +13,9 @@ def solution(x_success: int,
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    alpha = 0.06
-    mean1 = x_success/ x_cnt
-    mean2 = y_success/ y_cnt
-    return mean2-mean1 < alpha
+    prop_control = x_success / x_cnt
+    prop_test = y_success / y_cnt
+
+    z_stat, p_value = proportions_ztest([x_success, y_success], [x_cnt, y_cnt], alternative='larger')
+
+    return z_stat > 1.555
